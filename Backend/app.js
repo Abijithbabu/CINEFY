adb3e4157e70cb0cff0c858421f6eb21a384c6c6
Abhijith Babu <abijithb2002@gmail.com>
2023-07-23 22:35:22 +0530

    database changed to atlas

--- Backend/app.js
+++ Backend/app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use('/api',router)
 app.use('/api/admin',adminRouter)
 
-mongoose.connect('mongodb://127.0.0.1:27017/ArtistoClub').then(()=>{
+mongoose.connect(process.env.MONGODB_URL).then(()=>{
     app.listen(5000)
     console.log('Database connected in port 5000')
-}).catch((err) => console.log(err))
+}).catch((err) => console.log(err)) 
